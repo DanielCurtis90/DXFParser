d196372751ae91666e60aab9585c815d7976c2d0
Daniel Curtis <danielcurtis0@gmail.com>
2017-11-16 23:45:57 +0000

    Added Block Sub-entity parsing, increased the amount of entity types parsed

--- dxfparser.py
+++ dxfparser.py
@@ -1,6 +1,7 @@
 import ezdxf
 from entityprocessor import *
 
+
 #parsed_dxf is a Drawing object in ezdxf
 parsed_dxf = ezdxf.readfile("ScrapeMe.dxf")
 
@@ -8,32 +9,28 @@ modelspace = parsed_dxf.modelspace()
 
 layers = parsed_dxf.layers
 
-#we're comparing the layer name to the entity name to extract entities layer by layer
+#Iterate through the Block space and extract the Entities within them
+#First go through each block within the dxf file
 with open("output.txt", "w") as outputfile:
-    	for L in layers:
-    		L.on()
-    		outputfile.write(f"Layer name: {L.dxf.name} \n")
-    		for e in modelspace:
-    			if e.dxf.layer == L.dxf.name:
-    				outputinfo = entity_processor(e)
-    				outputfile.write(f"Entity Type: {e.dxftype()} \n")
-    				outputfile.write(f"Attributes:  {outputinfo} \n")
+    for block in parsed_dxf.blocks:
+        outputfile.write(f"Block Name: {block.name} \n")
+        #Now look through the entities within each block
+        for block_entity in block:
+            #Now classify and extract each subentity in each block
+            outputinfo = entity_processor(block_entity)
+            outputfile.write(f"Block-SubEntity Type: {block_entity.dxftype()} \n")
+            outputfile.write(f"Attributes:  {outputinfo} \n")
 
 
+#We're comparing the layer name to the entity name to extract entities layer by layer
+with open("output.txt", "a") as outputfile:
+    outputfile.write(f"Modelspace Entity Data by Layer")	
+    for L in layers:
+        L.on()
+        outputfile.write(f"Layer name: {L.dxf.name} \n")
+        for e in modelspace:
+            if e.dxf.layer == L.dxf.name:
+                outputinfo = entity_processor(e)
+                outputfile.write(f"Entity Type: {e.dxftype()} \n")
+                outputfile.write(f"Attributes:  {outputinfo} \n")
 
-
-#This outputs the entities and their type found in the model space (does not output any paper space)
-#with open("output.txt", "w") as outputfile:
-#    	for e in modelspace:
-#    		outputfile.write("DXF Entity: %s\n" % e.dxftype())
-			
-#block_layout = parsed_dxf.blocks.__iter__()
-
-#with open("output.txt", "a") as outputfile:
-#	outputfile.write("Block Layout Information \n")
-#	for BL in block_layout:
-		
-		#Need to grab info from individual blocks too, make a list for each block layout of attributes.
-#		block_attibutes = BL.block.tag
-#		
-#		outputfile.write(f"Block Name: {BL.name} {block_attibutes}\n")
--- entityprocessor.py
+++ entityprocessor.py
@@ -3,12 +3,63 @@ import ezdxf
 def entity_processor(entity):
 	if entity.dxftype() == 'LWPOLYLINE':
 		return lwpolyline_extractor(entity)
+	elif entity.dxftype() == 'INSERT':
+		return insert_extractor(entity)
+	elif entity.dxftype() == 'LINE':
+		return line_extractor(entity)
+	elif entity.dxftype() == 'MTEXT':
+		return mtext_extractor(entity)
+	else:
+		return "No Extractor defined for this Entity type"
 
 def lwpolyline_extractor(entity):
-	polyattr_list = []
-	polyattr_list.append(f"Elevation: {entity.dxf.elevation}, ")
-	polyattr_list.append(f"Point Information: ")
+	attr_list = []
+	attr_list.append(f"Elevation: {entity.dxf.elevation}, ")
+	attr_list.append(f"Constant Line Width: {entity.dxf.const_width}, ")
+	attr_list.append(f"Number of Vertices: {entity.dxf.count}, ")
+	attr_list.append(f"Point Information: ")
 	for poly_point in entity.get_points():
-		polyattr_list.append(f"Point: {poly_point}, ")
-	polyattr_list.append(f"Is shape closed?: {entity.closed} ")
-	return polyattr_list
+		attr_list.append(f"Point: {poly_point}, ")
+	attr_list.append(f"Is shape closed?: {entity.closed} ")
+	return attr_list
+
+def insert_extractor(entity):
+	insertattr_list = []
+	insertattr_list.append(f"Block Name: {entity.dxf.name}, ")
+	insertattr_list.append(f"Line Type: {entity.dxf.linetype}, ")
+	insertattr_list.append(f"Color: {entity.dxf.color}, ")
+	insertattr_list.append(f"Scaling Factor (X): {entity.dxf.xscale}, ")
+	insertattr_list.append(f"Scaling Factor (Y): {entity.dxf.yscale}, ")
+	insertattr_list.append(f"Scaling Factor (Z): {entity.dxf.zscale}, ")
+	insertattr_list.append(f"Rotation: {entity.dxf.rotation}, ")
+	insertattr_list.append(f"Repeated Row Insertion Count: {entity.dxf.row_count}, ")
+	insertattr_list.append(f"Repeated Row Insertion Spacing: {entity.dxf.row_spacing}, ")
+	insertattr_list.append(f"Repeated Column Insertion Count: {entity.dxf.column_count}, ")
+	insertattr_list.append(f"Repeated Column Insertion Spacing: {entity.dxf.column_spacing}, ")
+	insertattr_list.append(f"Insertion Point: {entity.dxf.insert}")
+	return insertattr_list
+
+def line_extractor(entity):
+	attr_list = []
+	attr_list.append(f"Start Point: {entity.dxf.start}, ")
+	attr_list.append(f"End Point: {entity.dxf.end}")
+	return attr_list
+
+def mtext_extractor(entity):
+	attr_list = []
+	attr_list.append(f"Text: {entity.get_text()}, ")
+	attr_list.append(f"Insertion Point: {entity.dxf.insert}, ")
+	attr_list.append(f"Character Height: {entity.dxf.char_height}, ")
+	attr_list.append(f"Width: {entity.dxf.width}, ")
+	attr_list.append(f"Attachment Point Flag: {entity.dxf.attachment_point}, ")
+	attr_list.append(f"Text Flow Direction Flag: {entity.dxf.flow_direction}, ")
+	attr_list.append(f"Style: {entity.dxf.style}, ")
+	if hasattr(entity, 'text_direction'):
+		attr_list.append(f"Text Direction (Overrides Rotation!): {entity.dxf.text_direction}, ")
+	attr_list.append(f"Rotation: {entity.dxf.rotation}, ")
+	attr_list.append(f"Line Spacing Style Flag: {entity.dxf.line_spacing_style}, ")
+	attr_list.append(f"Line Spacing Factor: {entity.dxf.line_spacing_factor}, ")
+	return attr_list
+
+
+
